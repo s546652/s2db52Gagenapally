ce94ec1d5cb4d67cf04be6e6420ff3315016a3a7
s546652 <98067009+s546652@users.noreply.github.com>
2022-04-21 15:24:03 -0500

    changes related to screenshot 2/3 lab 13

--- app.js
+++ app.js
@@ -2,7 +2,34 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+
+
 
 const connectionString =
 process.env.MONGO_CON
@@ -72,6 +99,18 @@ app.use('/house',houseRouter);
 app.use('/addmods',addmodsRouter);
 app.use('/selector',selectorRouter);
 app.use('/resource',resourceRouter);
+
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account')); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
